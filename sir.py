855c95201acf08c39e8d8a1a8ef6b6d9bed9964c
Marcelo Gasparian Gosling <marcelo.gosling@gmail.com>
2020-03-26 20:25:55 -0300

    Solução do modelo SIR rodando

--- sir.py
+++ sir.py
@@ -4,48 +4,28 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 from scipy import integrate
 
-def temp_sir(initial=(210_000_000., 2200., 100.), beta=0.1, mu=14):
-    S,I,R = initial
-    return
-
-def solve_sir(sigma=10.0,  =8./3, rho=28.0):
-    """Plot a solution to the Lorenz differential equations."""
-
-    max_time = 4.0
-    N = 30
-
-    fig = plt.figure()
-    ax = fig.add_axes([0, 0, 1, 1], projection='3d')
-    ax.axis('off')
-
-    # prepare the axes limits
-    ax.set_xlim((-25, 25))
-    ax.set_ylim((-35, 35))
-    ax.set_zlim((5, 55))
+def solve_sir(S_i=210_000_000., I_i=2200., R_i=100., beta=0.15, mu=14):
+    """Plot a solution to the SIR differential equations."""
+    max_time = 100.
+    N = 24
     
-    def lorenz_deriv(x_y_z, t0, sigma=sigma, beta=beta, rho=rho):
+#     fig = plt.figure()
+#     ax = fig.add_axes([0, 0, 1, 1])
+    
+#     ax.set_xlim((0, max_time))
+    
+    def sir_deriv(s_i_r, t0, beta=beta, mu=mu):
         """Compute the time-derivative of a Lorenz system."""
-        x, y, z = x_y_z
-        return [sigma * (y - x), x * (rho - z) - y, x * y - beta * z]
-
-    # Choose random starting points, uniformly distributed from -15 to 15
-    np.random.seed(1)
-    x0 = -15 + 30 * np.random.random((N, 3))
-
-    # Solve for the trajectories
-    t = np.linspace(0, max_time, int(250*max_time))
-    x_t = np.asarray([integrate.odeint(lorenz_deriv, x0i, t)
-                      for x0i in x0])
+        s, i, r = s_i_r
+        n = s + i + r
+        return [
+            -beta * i * s/n, 
+            beta * i * s/n - i/mu, 
+            i/mu
+        ]
     
-    # choose a different color for each trajectory
-    colors = plt.cm.viridis(np.linspace(0, 1, N))
-
-    for i in range(N):
-        x, y, z = x_t[i,:,:].T
-        lines = ax.plot(x, y, z, '-', c=colors[i])
-        plt.setp(lines, linewidth=2)
-    angle = 104
-    ax.view_init(30, angle)
+    t = np.linspace(0,max_time,int(N*max_time))
+    s_i_r_t = integrate.odeint(sir_deriv, (S_i, I_i, R_i), t)
+    plt.plot(t, s_i_r_t)
     plt.show()
-
-    return t, x_t
+    return t, s_i_r_t
